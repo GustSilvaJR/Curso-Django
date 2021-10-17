677c51d9a6c326765572a75c28d53ad0f0185cad
Gustavo Alessandro <gust.offic@gmail.com>
2021-10-17 13:16:50 -0300

    Curso

---
+++ usuarios/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractUserManager, PermissionsMixin)
+
+# Create your models here.
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self,email,password=None):
+        usuario = self.model(
+            email = self.normalize_email(email)
+        )
+
+        usuario.is_active = True
+        usuario.is_staff = False
+        usuario.is_superuser = False
+
+        if password:
+            usuario.set_password(password)
+        
+        usuario.save()
+
+        return usuario
+
+    def create_superuser(self,email,password):
+        usuario = self.model(
+            email = self.normalize_email(email),
+            password = password
+        )        
+
+        usuario.is_active = True
+        usuario.is_staff = True
+        usuario.is_superuser = True
+
+        usuario.set_password(password)
+
+        usuario.save()
+
+        return usuario
+
+
+
+class Usuario(AbstractUserManager, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="E-mail do usuário",
+        max_length=194,
+        unique=True
+    )
+
+    is_active = models.BooleanField(
+        verbose_name="Usuário ativo",
+        default=True,   
+    )
+
+    is_staff = models.BooleanField(
+        verbose_name = "Usuário é desenvolvedor",
+        default=True,
+    )
+
+    is_superuser = models.BooleanField(
+        verbose_name="Usuário é um superusuário",
+        default=True,
+    )
+
+    USERNAME_FIELD = 'email' # informando que o modo de login no admin do django vai ser pelo atributo email que adicionei
+
+    objects = UsuarioManager()
+
+    class Meta:
+        verbose_name = "Usuário",
+        verbose_name_plural = "Usuários",
+        db_table = "usuario"
+
+    def __str__ (self):
+        return self.email    
